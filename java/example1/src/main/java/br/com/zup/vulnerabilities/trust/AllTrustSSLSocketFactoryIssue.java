d8932b237707c9e9cfc338153e7b7ba09f48df53
Otavio Santana <otaviopolianasantana@gmail.com>
2021-09-23 10:54:34 +0100

    Defines the left classes to the all trust package
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- java/example1/src/main/java/br/com/zup/vulnerabilities/trust/AllTrustSSLSocketFactoryIssue.java
+++ java/example1/src/main/java/br/com/zup/vulnerabilities/trust/AllTrustSSLSocketFactoryIssue.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package br.com.zup.vulnerabilities;
+package br.com.zup.vulnerabilities.trust;
 
 import java.io.IOException;
 import java.net.InetAddress;
